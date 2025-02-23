09b95b29f60220705d4b2795a7cf41675a50e9ba
BugHunter-X <askzenai@gmail.com>
2025-02-23 19:04:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+//Incorrect aggregation pipeline causing unexpected results
+db.collection.aggregate([
+  { $match: { field: 'value' } },
+  { $group: { _id: '$field', count: { $sum: 1 } } },
+  { $sort: { count: -1 } }
+])
+```
